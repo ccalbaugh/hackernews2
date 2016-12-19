0e9f8b2232d6c5c91699167f360aa56e08c65e08
Caden Albaugh <CadenAlbaugh@gmail.com>
2016-12-19 08:59:02 -0600

    Move SignupDialog class to new file.

---
+++ src/SignupDialog.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+
+const Dialog = ({ title, message, children }) => {
+  return (
+    <div className="Dialog">
+      <h1 className="Dialog-title">
+        {title}
+      </h1>
+      <p className="Dialog-message">
+        {message}
+      </p>
+      {children}
+    </div>
+  );
+}
+
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {login: ''};
+  }
+
+  render() {
+    return(
+      <Dialog title="Mars Exploration Program"
+              message="How should we refer to you?">
+        <input value={this.state.login}
+               onChange={this.handleChange} />
+        <button onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </Dialog>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({login: e.target.value});
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
+}
+
+export default SignUpDialog;

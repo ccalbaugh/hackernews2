ce599f27766484dbf8001aa56ddf75df8e7a39f0
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-03 09:44:58 -0600

    Add snapshots directory and Search test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from './App';
+import { Search, Button, Table } from './App';
 
 describe('App', () => {
 
@@ -20,4 +20,41 @@ describe('App', () => {
 
 });
 
+describe('Search', () => {
+
+	it('renders', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Search>Search</Search>, div);
+	});
+
+	test('snapshots', () => {
+		const component = renderer.create(
+			<Search>Search</Search>
+		);
+		let tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+});
+
+// describe('Button', () => {
+
+// 	it('renders', () => {
+// 		const div = document.createElement('div');
+// 		ReactDOM.render(<Button>Give Me More</Button>, div);
+// 	});
+
+// 	test('snapshots', () => {
+// 		const component = renderer.create(
+// 			<Button>Give Me More</Button>
+// 		);
+// 		let tree = component.toJSON();
+// 		expect(tree).toMatchSnapshot();
+// 	});
+
+// });
+
+// describe('Table', () => {
+// 	const props
+// })
 

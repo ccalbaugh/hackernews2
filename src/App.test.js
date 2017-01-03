4e242fc30e36aa16ae111ce2cf8af55f3f0b47d9
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-03 09:48:36 -0600

    Create test for table component

--- src/App.test.js
+++ src/App.test.js
@@ -54,7 +54,27 @@ describe('Button', () => {
 
 });
 
-// describe('Table', () => {
-// 	const props
-// })
+describe('Table', () => {
+
+	const props = {
+		list: [
+			{ title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+			{ title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+		],
+	};
+
+	it('renders', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Table { ...props } />, div);
+	});
+
+	test('snapshots', () => {
+		const component = renderer.create(
+			<Table { ...props } />
+		);
+		let tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+});
 
